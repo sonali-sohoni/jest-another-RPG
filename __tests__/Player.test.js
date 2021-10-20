ec047de2037ebcb0647fb68d2441ac3aef810c83
sonali_sohoni <ssohoni@camsbycbs.com>
2021-10-20 16:01:14 -0400

    player object creation and testing

---
+++ __tests__/Player.test.js
@@ -0,0 +1,16 @@
+const { TestWatcher } = require("@jest/core");
+const Player = require("../lib/Player");
+const Potion = require("../lib/Potion");
+
+jest.mock("../lib/Potion.js");
+console.log(new Potion());
+test("Test the player object", () => {
+	const player = new Player("Dave");
+	expect(player.name).toBe("Dave");
+	expect(player.health).toEqual(expect.any(Number));
+	expect(player.agility).toEqual(expect.any(Number));
+	expect(player.strength).toEqual(expect.any(Number));
+	expect(player.inventory).toEqual(
+		expect.arrayContaining([expect.any(Object)])
+	);
+});
